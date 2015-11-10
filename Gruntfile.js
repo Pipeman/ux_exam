c78ef649d8d26825f919e302d7b68a0c1f46567f
TozzY_dev <TozzY_dev@MacBook-Pro-di-TozzY.local>
2015-11-10 15:47:29 +0100

    build of the app

--- Gruntfile.js
+++ Gruntfile.js
@@ -352,7 +352,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'tozzYDevApp',
+          module: 'uxExam',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
